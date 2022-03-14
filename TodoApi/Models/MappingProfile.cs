6a297393234e56efa6dbfa575ffeae187e5e8806
wacb14@gmail.com <wacb14@gmail.com>
2022-03-14 11:10:07 -0500

    Modificando la informacion con el mapping

--- TodoApi/Models/MappingProfile.cs
+++ TodoApi/Models/MappingProfile.cs
@@ -1,11 +1,15 @@
 using AutoMapper;
 using TodoApi.ViewModels;
 
-namespace TodoApi.Models{
-    public class MappingProfile : Profile{
-        public MappingProfile(){
-            CreateMap<User,UserViewModel>();
-            CreateMap<UserViewModel,User>();
+namespace TodoApi.Models
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<User, UserViewModel>()
+            .ForMember(dest => dest.UserName, opt => opt.MapFrom(src => src.UserName.Substring(0, src.UserName.Length - 3)));
+            CreateMap<UserViewModel, User>();
         }
     }
 }

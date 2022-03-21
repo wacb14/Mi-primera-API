f50407833c8ca35b9a34e6284474f15ae0d0b8ba
wacb14@gmail.com <wacb14@gmail.com>
2022-03-21 17:47:22 -0500

    Solucionado el CORS para conexion con Angular

--- TodoApi/Startup.cs
+++ TodoApi/Startup.cs
@@ -26,6 +26,7 @@ namespace TodoApi
 {
     public class Startup
     {
+        readonly string MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -36,7 +37,15 @@ namespace TodoApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddCors(options=>{
+                options.AddPolicy(name: MyAllowSpecificOrigins, 
+                    builder=>{
+                        builder.WithOrigins("http://localhost:4200")
+                        .AllowAnyHeader()
+                        .AllowAnyMethod();
+                    }
+                 );
+            });
             services.AddControllers();
             services.AddTransient<IUserDataService, UserDataService>();
             services.AddTransient<IUserBusinessService, UserBusinessService>();
@@ -130,6 +139,8 @@ namespace TodoApi
             app.UseHttpsRedirection();
 
             app.UseRouting();
+            
+            app.UseCors(MyAllowSpecificOrigins);
 
             app.UseAuthentication();
 
